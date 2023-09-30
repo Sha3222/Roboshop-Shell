109c6656939079124ec2ccdfbfb1b5212a27cc7c
Bablu <bablu.chilukoti@gmail.com>
2023-09-30 21:00:03 +0530

    added mysql

---
+++ mysql.sh
@@ -0,0 +1,7 @@
+cp sqlrepo /etc/yum.repos.d/mysql.repo
+yum module disable mysql -y
+yum install mysql-community-server -y
+systemctl enable mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass RoboShop@1
+mysql -uroot -pRoboShop@1
