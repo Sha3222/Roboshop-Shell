1368f239864de691066f60c9996e02f7c3b5928c
Bablu <bablu.chilukoti@gmail.com>
2023-10-16 17:52:23 +0530

    updated

--- catalogue.sh
+++ catalogue.sh
@@ -2,18 +2,13 @@ cp catalogueService /etc/systemd/system/catalogue.service
 cp mongo.Repo /etc/yum.repos.d/mongo.repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-
 useradd roboshop
-
 mkdir /app
-
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
-
 cd /app
 npm install
-
 yum install mongodb-org-shell -y
 mongo --host mongodb.sreddy.online </app/schema/catalogue.js
 
