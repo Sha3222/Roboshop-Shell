e8dafabb1a0ce9258501be80c120084570bf327e
Bablu <bablu.chilukoti@gmail.com>
2023-10-22 19:56:42 +0530

    added /tmp/robofile.log

--- catalogue.sh
+++ catalogue.sh
@@ -1,26 +1,26 @@
-echo -e "\e[34m<<<<Creating the Catalogue service>>>>>>>>>>>>\e[0m" &>/temp/robofile
-cp catalogueService /etc/systemd/system/catalogue.service
-echo -e "\e[34m<<<<copying Mongodb repo file>>>>>>>>>>>>>\e[0m" &>/temp/robofile
-cp mongo.Repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[34m<<<<<<<installing Nodejs>>>>>>>>>>>>>>>>>>>>>\e[0m" &>/temp/robofile
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-echo -e "\e[34m<<<<Adding Roboshop user>>>>>>>>>>>>>>>>>>\e[0m" &>/temp/robofile
-useradd roboshop
-echo -e "\e[34m<<<<<<<<<<<<<<<<<removing app>>>>>>>>>>>>>>>>>>>>>>\e[0m" &>/temp/robofile
-rm -rf /app
-echo -e "\e[34m<<<<creating app directory>>>>>>>>>>>>>>>>>>>>>\e[0m" &>/temp/robofile
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-echo -e "\e[34m<<<<<insatlling mongod>>>>>>>>>>>>>>>>>>>\e[0m" &>/temp/robofile
-yum install mongodb-org-shell -y
-echo -e "\e[34m<<<<loading schema>>>>>>>\e[0m" &>/temp/robofile
-mongo --host mongodb.sreddy.online </app/schema/catalogue.js
-echo -e "\e[34m<<<<starting the catlogue service>>>>>>>>>>>>>>>>>>>\e[0m" &>/temp/robofile
+echo -e "\e[34m<<<<Creating the Catalogue service>>>>>>>>>>>>\e[0m"
+cp catalogueService /etc/systemd/system/catalogue.service &>/tmp/robofile.log
+echo -e "\e[34m<<<<copying Mongodb repo file>>>>>>>>>>>>>\e[0m"
+cp mongo.Repo /etc/yum.repos.d/mongo.repo &>/tmp/robofile.log
+echo -e "\e[34m<<<<<<<installing Nodejs>>>>>>>>>>>>>>>>>>>>>\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>/tmp/robofile.log
+yum install nodejs -y &>/tmp/robofile.log
+echo -e "\e[34m<<<<Adding Roboshop user>>>>>>>>>>>>>>>>>>\e[0m"
+useradd roboshop &>/tmp/robofile.log
+echo -e "\e[34m<<<<<<<<<<<<<<<<<removing app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+rm -rf /app &>/tmp/robofile.log
+echo -e "\e[34m<<<<creating app directory>>>>>>>>>>>>>>>>>>>>>\e[0m"
+mkdir /app &>/tmp/robofile.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>/tmp/robofile.log
+cd /app &>/tmp/robofile.log
+unzip /tmp/catalogue.zip &>/tmp/robofile.log
+cd /app &>/tmp/robofile.log
+npm install &>/tmp/robofile.log
+echo -e "\e[34m<<<<<insatlling mongod>>>>>>>>>>>>>>>>>>>\e[0m"
+yum install mongodb-org-shell -y &>/tmp/robofile.log
+echo -e "\e[34m<<<<loading schema>>>>>>>\e[0m"
+mongo --host mongodb.sreddy.online </app/schema/catalogue.js &>/tmp/robofile.log
+echo -e "\e[34m<<<<starting the catlogue service>>>>>>>>>>>>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
