f01b4b848ffcda47cb59f4d44722c466a1147fb9
Bablu <bablu.chilukoti@gmail.com>
2023-10-22 19:43:55 +0530

    added color code

--- catalogue.sh
+++ catalogue.sh
@@ -1,26 +1,26 @@
-echo "<<<<Creating the Catalogue service>>>>>"
+echo "\e[34m<<<<Creating the Catalogue service>>>>>>>>>>>>\e[0m"
 cp catalogueService /etc/systemd/system/catalogue.service
-echo "<<<<copying Mongodb repo file>>>>>"
+echo "\e[34m<<<<copying Mongodb repo file>>>>>>>>>>>>>\e[0m"
 cp mongo.Repo /etc/yum.repos.d/mongo.repo
-echo "<<<<<<<installing Nodejs>>>>>>>>"
+echo "\e[34m<<<<<<<installing Nodejs>>>>>>>>>>>>>>>>>>>>>\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-echo "<<<<Adding Roboshop user>>>>>>"
+echo "\e[34m<<<<Adding Roboshop user>>>>>>>>>>>>>>>>>>\e[0m"
 useradd roboshop
-echo "<<<<<<<<<<<<<<<<<removing app>>>>>>>>>>>>>>>>>>>>>>"
+echo "\e[34m<<<<<<<<<<<<<<<<<removing app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 rm -rf /app
-echo "<<<<creating app directory>>>>>"
+echo "\e[34m<<<<creating app directory>>>>>>>>>>>>>>>>>>>>>\e[0m"
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 npm install
-echo "<<<<<insatlling mongod>>>>"
+echo "\e[34m<<<<<insatlling mongod>>>>>>>>>>>>>>>>>>>\e[0m"
 yum install mongodb-org-shell -y
-echo "<<<<loading schema>>>"
+echo "\e[34m<<<<loading schema>>>"
 mongo --host mongodb.sreddy.online </app/schema/catalogue.js
-echo "<<<<starting the catlogue service>>>>>"
+echo "\e[34m<<<<starting the catlogue service>>>>>>>>>>>>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
