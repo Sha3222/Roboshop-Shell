ec86b8c8c860c4066ef2843b4b17d4b2fe5de00f
Bablu <bablu.chilukoti@gmail.com>
2023-10-22 19:46:30 +0530

    added color code

--- catalogue.sh
+++ catalogue.sh
@@ -1,26 +1,26 @@
-echo "\e[34m<<<<Creating the Catalogue service>>>>>>>>>>>>\e[0m"
+echo -e "\e[34m<<<<Creating the Catalogue service>>>>>>>>>>>>\e[0m"
 cp catalogueService /etc/systemd/system/catalogue.service
-echo "\e[34m<<<<copying Mongodb repo file>>>>>>>>>>>>>\e[0m"
+echo -e "\e[34m<<<<copying Mongodb repo file>>>>>>>>>>>>>\e[0m"
 cp mongo.Repo /etc/yum.repos.d/mongo.repo
-echo "\e[34m<<<<<<<installing Nodejs>>>>>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[34m<<<<<<<installing Nodejs>>>>>>>>>>>>>>>>>>>>>\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-echo "\e[34m<<<<Adding Roboshop user>>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[34m<<<<Adding Roboshop user>>>>>>>>>>>>>>>>>>\e[0m"
 useradd roboshop
-echo "\e[34m<<<<<<<<<<<<<<<<<removing app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[34m<<<<<<<<<<<<<<<<<removing app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 rm -rf /app
-echo "\e[34m<<<<creating app directory>>>>>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[34m<<<<creating app directory>>>>>>>>>>>>>>>>>>>>>\e[0m"
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 npm install
-echo "\e[34m<<<<<insatlling mongod>>>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[34m<<<<<insatlling mongod>>>>>>>>>>>>>>>>>>>\e[0m"
 yum install mongodb-org-shell -y
-echo "\e[34m<<<<loading schema>>>"
+echo -e "\e[34m<<<<loading schema>>>"
 mongo --host mongodb.sreddy.online </app/schema/catalogue.js
-echo "\e[34m<<<<starting the catlogue service>>>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[34m<<<<starting the catlogue service>>>>>>>>>>>>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
