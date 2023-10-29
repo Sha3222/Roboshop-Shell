4bed69a4ccb20296dc5e7954f88bc2d9ed0eb787
Bablu <bablu.chilukoti@gmail.com>
2023-10-29 21:55:20 +0530

    Added exit status

--- common.sh
+++ common.sh
@@ -1,24 +1,39 @@
 log=/tmp/robofile.log
+exit_fun () {
+  if [ $? -eq 0 ]
+  then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
+}
 App_prerequest_function () {
     log=/tmp/robofile.log
     echo -e "\e[34m<<<<Creating ${variable} service>>>>>>>>>>>>\e[0m"
     cp Service /etc/systemd/system/payment.service &>>${log}
+    exit_fun
+
     echo -e "\e[34m >>>>>>>>>>>>>Adding User>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
     useradd roboshop
+    exit_fun
 
     echo -e "\e[34m<<<<<<<<<<<<<<<<<removing app>>>>>>>>>>>>>>>>>>>>>>\e[0m"
     rm -rf /app &>>${log}
+    exit_fun
 
     echo -e "\e[34m<<<<creating app directory>>>>>>>>>>>>>>>>>>>>>\e[0m"
     mkdir /app &>>${log}
+    exit_fun
 
     echo -e "\e[34m >>>>>>>>>>>>>Downloading the Application Content>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
     curl -L -o /tmp/${varible}.zip https://roboshop-artifacts.s3.amazonaws.com/${variable}.zip &>>${log}
+    exit_fun
 
     echo -e "\e[34m >>>>>>>>>>>>>Extracting the Application File>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
     cd /app
     unzip /tmp/${varible}.zip &>>${log}
     cd /app &>>{log}
+    exit_fun
 
 }
 
@@ -27,6 +42,7 @@ systemd_function () {
   systemctl daemon-reload &>>${log}
   systemctl enable ${variable} &>>${log}
   systemctl start ${variable} &>>${log}
+  exit_fun
 }
 
 schema_fun () {
@@ -34,7 +50,9 @@ schema_fun () {
   then
     echo -e "\e[34m >>>>>>>>>>>>>Installation Mongodb Client>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
     dnf install mongodb-org-shell -y &>>${log}
+    exit_fun
     mongo --host mongodb.sreddy.online </app/schema/${varible}.js &>>${log}
+    exit_fun
   fi
 
   if [ "$schema_type" == mysql ]
@@ -42,6 +60,7 @@ schema_fun () {
    echo -e "\e[34m<<<<Loading the Sql Schema>>>>>>>>>>>>\e[0m"
    yum install mysql -y &>>${log}
    mysql -h mysql.sreddy.online -uroot -pRoboShop@1 < /app/schema/${variable}.sql &>>${log}
+   exit_fun
   fi
 
 }
@@ -51,6 +70,7 @@ Node_js () {
   then
     echo -e "\e[34m >>>>>>>>>>>>>Mongodb Repo file>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
     cp mongo.Repo /etc/yum.repos.d/mongo.repo
+    exit_fun
   fi
 
   echo -e "\e[34m >>>>>>>>>>>>>Installation Node JS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
@@ -61,6 +81,7 @@ Node_js () {
   echo -e "\e[34m >>>>>>>>>>>>>Mongodb dependencies>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
   cd /app
   npm install &>>${log}
+  exit_fun
 
   schema_fun
 
@@ -70,10 +91,14 @@ Node_js () {
 
 Catalogue () {
   log=/tmp/robofile.log
+
   echo -e "\e[34m<<<<Creating the ${variable}service>>>>>>>>>>>>\e[0m"
   cp ${variable}Service /etc/systemd/system/${variable}.service
+  exit_fun
+
   echo -e "\e[34m<<<<copying Mongodb repo file>>>>>>>>>>>>>\e[0m"
   cp mongo.Repo /etc/yum.repos.d/mongo.repo &>>${log}
+  exit_fun
 
   App_prerequest_function
 
@@ -81,9 +106,11 @@ Catalogue () {
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${log}
   yum install nodejs -y &>>${log}
   npm install &>>${log}
+  exit_fun
 
   echo -e "\e[34m<<<<<insatlling mongod>>>>>>>>>>>>>>>>>>>\e[0m"
   yum install mongodb-org-shell -y &>>${log}
+  exit_fun
 
   schema_fun
 
@@ -94,14 +121,18 @@ java_shipping () {
 
   echo -e "\e[34m<<<<Mysql repo>>>>>>>>>>>>\e[0m"
   cp sqlrepo /etc/yum.repos.d/mysql.repo &>>${log}
+  exit_fun
 
   echo -e "\e[34m<<<<Installing Mavan>>>>>>>>>>>>\e[0m"
   yum install maven -y &>>${log}
+  exit_fun
 
   App_prerequest_function
 
   mvn clean package
+  exit_fun
   mv target/${variable}-1.0.jar ${variable}.jar &>>${log}
+  exit_fun
 
   schema_fun
 
@@ -122,13 +153,17 @@ python_payment () {
 
 dispatch_fun () {
   log=/tmp/robofile.log
+
   echo -e "\e[34m<<<<Go Installation>>>>>>>>>>>>\e[0m"
   yum install golang -y &>>${log}
+  exit_fun
 
   App_prerequest_function
 
   go mod init dispatch
+  exit_fun
   go get &>>${log}
+  exit_fun
   go build &>>${log}
 
   systemd_function
