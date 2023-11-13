7bf32442fb0619c2b10b9a2f155be9b9f4a3b780
Bablu <bablu.chilukoti@gmail.com>
2023-11-13 17:07:09 +0530

    MySql

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Roboshop-Shell
+
+VARIABLES
+When we have some repetative content then we can declare that in 
+variable and we can use that variable reference every where. 
+Advantages is, if we change in place that impacts all the places
+where that value is used.
+a=10
+name=sha
+>Bash shell don't have the data types(10,Sha)
+>Simply we can declare value
+Variables names (a,name) should be using alphabets
+and numbers and underscore
+>if values having any special characters then double quote it.
+>Access variable with $ as prefix and also optionally variable
+name in flower braces
+$var 0r ${var}
+
+Special Variables ranges fro inputs
+$0-$n-Arguments
+$* for all values
+$# for no. of Arguments
+0$ to know the script name
+
+
+Command Substitution variable is declared by using command output
+var=$(command)
+
+Artimetic Substitution variable is declared by solving arthimetic expression
+var=$((expression))
+
+today_date=$(date)
+add=$((2+3))
--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,26 @@
-cp sqlrepo /etc/yum.repos.d/mysql.repo
-yum module disable mysql -y
-yum install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+source common.sh
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]
+then
+  echo Input password missing
+  exit 1
+fi
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>Copying the mysql repo file>>>>>>>>>>>>>>>>>>>>\e[0m"
+cp sqlrepo /etc/yum.repos.d/mysql.repo &>>${log}
+exit_fun
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>Module disable >>>>>>>>>>>>>>>>>>>>\e[0m"
+yum module disable mysql -y &>>${log}
+exit_fun
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>Installing Mysql>>>>>>>>>>>>>>>>>>>>\e[0m"
+yum install mysql-community-server -y &>>${log}
+exit_fun
+
+echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>Starting mysql service>>>>>>>>>>>>>>>>>>>>\e[0m"
+systemctl enable mysqld &>>${log}
+systemctl start mysqld &>>${log}
+exit_fun
+
+mysql_secure_installation --set-root-pass ${mysql_root_password}
 mysql -uroot -pRoboShop@1
